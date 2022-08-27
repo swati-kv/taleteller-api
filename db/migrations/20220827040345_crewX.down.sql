ecbe22d4ce1306f0409eb4e0ec2a8d151688daf0
padmaraj-kv <padmaraj@keyvalue.systems>
2022-08-27 11:33:50 +0530

    Added migrations

---
+++ db/migrations/20220827040345_crewX.down.sql
@@ -0,0 +1,19 @@
+ALTER TABLE "story" DROP CONSTRAINT "story_scene_id";
+
+ALTER TABLE "story" DROP CONSTRAINT "story_customer_id";
+
+ALTER TABLE "scene" DROP CONSTRAINT "scene_background_audio_id";
+
+ALTER TABLE "scene" DROP CONSTRAINT "scene_generated_audio_id";
+
+DROP TABLE "story";
+
+DROP TABLE "scene";
+
+DROP TABLE "background_audio";
+
+DROP TABLE "generated_audio";
+
+DROP TABLE "image";
+
+DROP TABLE "customer";
